5bf71045b009cf96311a3c3abb098cd0f111fa77
fuatio <fuat@fuat.io>
2022-06-29 19:35:55 -0400

    starting point

--- main.py
+++ main.py
@@ -1,41 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    author = db.Column(db.String(250), nullable=False)
-    rating = db.Column(db.Float, nullable=False)
-
-db.create_all()
-
-@app.route('/')
+@app.route("/")
 def home():
-    all_books = db.session.query(Book).all()
-    return render_template("index.html", all_books = all_books)
+    return render_template("index.html")
 
 
-@app.route("/add", methods=["GET", "POST"])
-def add():
-    if request.method == "POST":
-        new_book = Book(
-            title = request.form["title"],
-            author = request.form["author"],
-            rating = request.form["rating"],
-        )
-        db.session.add(new_book)
-        db.session.commit()
-        return redirect(url_for('home')) 
-    return render_template("add.html")
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
-
