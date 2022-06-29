8f8f924b04ff088a8cfa751f68452f6f2f980013
fuatio <fuat@fuat.io>
2022-06-29 16:41:26 -0400

    added sqlite syntax, data now persistent

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-all_books = [
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-]
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
 
+db.create_all()
 
 @app.route('/')
 def home():
+    all_books = db.session.query(Book).all()
     return render_template("index.html", all_books = all_books)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_book = {
-            "title" : request.form["title"],
-            "author" : request.form["author"],
-            "rating" : request.form["rating"]
-        }
-        all_books.append(new_book)
+        new_book = Book(
+            title = request.form["title"],
+            author = request.form["author"],
+            rating = request.form["rating"],
+        )
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for('home')) 
     return render_template("add.html")
 
